2c5545a14db3e62c544fd6b95287397966698df1
tomcat <risc604@ms40.url.com.tw>
2017-06-17 00:34:24 +0800

    datetime test function Ok

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+    static final String HEXES = "0123456789ABCDEF";
+
     public Main() {
     }
 
@@ -25,63 +29,59 @@ public class Main {
 	dfDemo.testDateFormat();
 	System.out.println("dfDemo: " + dfDemo.toString());
     }
-    
-    public static byte[] hexStringToByteArray(String s)
-    {
-        int len = s.length();
-        byte[] data = new byte[len/2];
 
-        for(int i = 0; i < len; i+=2)
-        {
-            data[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) +
-                                 Character.digit(s.charAt(i+1), 16));
-        }
+    public static byte[] hexStringToByteArray(String s) {
+	int len = s.length();
+	byte[] data = new byte[len / 2];
 
-        return data;
+	for (int i = 0; i < len; i += 2) {
+	    data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+	}
+
+	return data;
     }
 
-    public static String getHexToString(byte[] raw)
-    {
-        //StringBuilder sb= new StringBuilder(responInfo.length);
-        //for (byte indx: responInfo)
-        //{
-        //    sb.append(format("%02X", indx));
-        //}
+    public static String getHexToString(byte[] raw) {
+	// StringBuilder sb= new StringBuilder(responInfo.length);
+	// for (byte indx: responInfo)
+	// {
+	// sb.append(format("%02X", indx));
+	// }
 
-        if (raw == null)
-        {
-            return null;
-        }
+	if (raw == null) {
+	    return null;
+	}
 
-        final StringBuilder hex = new StringBuilder(2 * raw.length);
-        for (final byte b : raw)
-        {
-            hex.append(HEXES.charAt((b & 0xF0) >> 4))
-                    .append(HEXES.charAt((b & 0x0F)));
-        }
-        return hex.toString();
-    }
-    
-    public static int byteToUnsignedInt(byte b)
-    {
-        return 0x00 << 24 | b & 0xff;
+	final StringBuilder hex = new StringBuilder(2 * raw.length);
+	for (final byte b : raw) {
+	    hex.append(HEXES.charAt((b & 0xF0) >> 4)).append(HEXES.charAt((b & 0x0F)));
+	}
+	return hex.toString();
     }
 
-    public static void testDateTime()
-    {
-    		String hexDate = "1105160F23";
-    		byte[] byteHexDate = hexStringToByteArray(hexDate);
-    		System.out.println("byteHexDate: " + Arrays.);
-    		DateTime dt = new DateTime().withTime(5, 20, 0, 0);
+    public static int byteToUnsignedInt(byte b) {
+	return 0x00 << 24 | b & 0xff;
     }
 
+    public static void testHexDateTime() {
+	String hexDate = "1105160F23";
+	byte[] byteHexDate = hexStringToByteArray(hexDate);
+	System.out.println("byteHexDate: " + Arrays.toString(byteHexDate) + ", length: " + byteHexDate.length);
+	int[] intDate = new int[byteHexDate.length];
+	for (int i = 0; i < byteHexDate.length; i++) {
+	    intDate[i] = byteToUnsignedInt(byteHexDate[i]);
+	}
+	LocalDateTime dt = LocalDateTime.of(intDate[0], intDate[1], intDate[2], intDate[3], intDate[4], 0, 0);
+	System.out.println("dt: " + dt.toString() + ", " + dt.getYear() + "/" + dt.getMonthValue() + "/"
+		+ dt.getDayOfMonth() + "  " + +dt.getHour() + ":" + dt.getMinute() + ":" + dt.getSecond());
+    }
 
     public static void main(String[] args) throws ParseException {
 	// System.out.println("isNumeric(\"35.42\"): " + isNumeric("35.42"));
 	// System.out.println("isNumeric(\"35,42\"): " + isNumeric("35,42"));
 
 	// randomTest();
-	testDateTime();
+	testHexDateTime();
     }
 
 }
