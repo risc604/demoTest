f39f7d620e2a46997ea43103c20496d0b317ffe3
tomcat <tomcat.cheng@microlife.com.tw>
2017-12-06 11:11:20 +0800

    add local DateFormat test

--- src/LocaleDateTimeFormat.java
+++ src/LocaleDateTimeFormat.java
@@ -1,7 +1,10 @@
+import java.text.DateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class LocaleDateTimeFormat
 {
@@ -14,17 +17,25 @@ public class LocaleDateTimeFormat
 	public void getLocalDTFormat()
 	{
 		Calendar cal = Calendar.getInstance();
-		// Date date = cal.getTime();
-		System.out.println();
+		Date date = cal.getTime();
+		System.out.println("Date Obj: " + date.toString());
+		System.out.printf("Date: %tD %n", date);
 
+		System.out.println("Dateformat");
+		for (int i = 0; i < 4; i++)
+		{
+			DateFormat dtFormat = DateFormat.getDateTimeInstance(i, i, Locale.TAIWAN);
+			String showDT = dtFormat.format(date);
+			System.out.println("[" + i + "]: " + showDT);
+		}
 		DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDate ld = LocalDate.parse("2017-03-13", DATEFORMATTER);
 		LocalDateTime ldt = LocalDateTime.of(ld, LocalDateTime.now().toLocalTime());
 		System.out.println(ldt);
 
-		System.out.println(DATEFORMATTER.ofLocalizedDateTime(null));
+		// System.out.println(DATEFORMATTER.ofLocalizedDateTime(null));
 
-		// Locale locale = new Locale("en", "US"); // °Ï°ì¬°¬ü°ê
+		// Locale locale = new Locale("en", "US"); // ï¿½Ï°ì¬°ï¿½ï¿½ï¿½ï¿½
 		// DateFormat LocalFormat = DateFormat.getDateInstance(DateFormat.FULL, locale);
 		// System.out.println("US DT: " + Local);
 	}
