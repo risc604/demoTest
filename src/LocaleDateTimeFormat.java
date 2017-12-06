35b21e964e071d883dea61febcc5278469653c77
tomcat <tomcat.cheng@microlife.com.tw>
2017-12-06 17:28:12 +0800

    modify

--- src/LocaleDateTimeFormat.java
+++ src/LocaleDateTimeFormat.java
@@ -20,14 +20,14 @@ public class LocaleDateTimeFormat
 		Date date = cal.getTime();
 		System.out.println("Date Obj: " + date.toString());
 		System.out.printf("Date: %tD %n", date);
-
-		System.out.println("Dateformat");
+		System.out.println("Dateformat: ");
 		for (int i = 0; i < 4; i++)
 		{
 			DateFormat dtFormat = DateFormat.getDateTimeInstance(i, i, Locale.TAIWAN);
 			String showDT = dtFormat.format(date);
 			System.out.println("[" + i + "]: " + showDT);
 		}
+
 		DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		LocalDate ld = LocalDate.parse("2017-03-13", DATEFORMATTER);
 		LocalDateTime ldt = LocalDateTime.of(ld, LocalDateTime.now().toLocalTime());
